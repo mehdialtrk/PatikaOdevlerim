8927c6c0a6cd0db9ad0f927ae05dba9d88ae5a4b
Mehdi <87785177+mehdialtrk@users.noreply.github.com>
2022-01-10 23:41:23 +0300

    Add files via upload
    ödev oluşturuldu

---
+++ java/Metotlar/3-Üs Hesabı Yapan Program/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+                public static void main(String []args){
+                    power();
+                }
+
+        static void power() {
+                Scanner scan = new Scanner(System.in);
+                System.out.print("Taban değeri giriniz :");
+                int base = scan.nextInt();
+                System.out.print("Üs değeri giriniz :");
+                int exponent = scan.nextInt();
+                int result = 1;
+
+                for (int i = 1; i <= exponent; i++) {
+                result *= base;
+                }
+
+                System.out.println("Sonuç : " + result);
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
