5fc91232a923830028d15de916a168d44d61e166
Mehdi <87785177+mehdialtrk@users.noreply.github.com>
2022-01-30 21:02:17 +0300

    Add files via upload

---
+++ java/Metotlar/Desene Gore Metot/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] sayilar = {36, 58, 72, 48, 29, 61, 83, 41, 19, 27};
+        for (int i = 0; i < 10; i++) {
+            System.out.println(sayilar[i]);
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
