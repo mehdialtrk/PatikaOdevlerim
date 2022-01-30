902c2d221ca410a163c86bfeb370b1c85c896267
Mehdi <87785177+mehdialtrk@users.noreply.github.com>
2022-01-30 21:03:57 +0300

    Add files via upload

--- java/Metotlar/Desene Gore Metot/Main.java
+++ java/Metotlar/Desene Gore Metot/Main.java
@@ -1,19 +1,32 @@
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] sayilar = {36, 58, 72, 48, 29, 61, 83, 41, 19, 27};
-        for (int i = 0; i < 10; i++) {
-            System.out.println(sayilar[i]);
-        }
 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("N Sayisi: ");
+        int a=input.nextInt();
+        System.out.print(duzen(a));
 
 
     }
+    static int duzen(int a){
+        for (int i=a;i>-5;i-=5){
 
+            System.out.print(i+" ");
+
+        }
+        for(int j=a%5;j<a;j+=5){
+            if(j==0){
+                System.out.print("");
+            }
+            else
+
+                System.out.print(j+" ");
+        }
+
+        return a;
+    }
 
 }
-
-
-
-
